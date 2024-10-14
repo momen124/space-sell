b5b64c4fe92570e31352ba034ba8918b225254e7
HAdiiiir <hader.jaamal2010@gmail.com>
2024-10-14 09:19:42 +0300

    CategoriesPage

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,6 +1,6 @@
 // pages/categories/index.tsx
-import Header from '../../components/layout/Header';
-import Footer from '../../components/layout/Footer';
+import Header from '../components/layout/Header';
+import Footer from '../components/layout/Footer';
 import CategoryList from '../../components/categories/CategoryList';
 
 const CategoriesPage = () => {
